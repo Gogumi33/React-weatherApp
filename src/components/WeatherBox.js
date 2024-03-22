89337486a2fc2a2951d29b7d5724743388e0e87b
HyeongSeok Kim <king9833@korea.ac.kr>
2024-03-23 03:29:14 +0900

    modify fahrenheit

--- src/components/WeatherBox.js
+++ src/components/WeatherBox.js
@@ -4,13 +4,14 @@ import React from 'react'
 const WeatherBox = ({weather}) => { // ES6 문법 - 내가 갖고오고 싶은 내용의 키만 언급하면 뽑아올 수 있다.
   // console.log("날씨?", weather);
   let currentTemp = weather?.main.temp.toFixed(1);
+  let fahrenheit = currentTemp*9/5+32;
 
   return (
     <div className="weather-box">
       {/* 처음에는 null값이기 때문에 반드시 조건부를 하나 더 걸어줘서 걸러줘야함! */}
       {/* {weather?.name}도 가능하다. */}
       <div className="location">{weather && weather.name}</div>
-      <h2>{currentTemp}℃ / {currentTemp*9/5+32}℉</h2>
+      <h2>{currentTemp}℃ / {fahrenheit.toFixed(1)}℉</h2>
       <h3>{weather?.weather[0].description}</h3>
     </div>
   )
